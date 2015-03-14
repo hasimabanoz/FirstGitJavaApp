3f5a8c0d6e212e4824f7e675925d3da2ae984275
Haşim Abanoz <hasimabanoz@gmail.com>
2015-03-14 15:44:23 +0200

    created by haşim

---
+++ src/com/j32Bit/application/Application.java
@@ -0,0 +1,13 @@
+package com.j32Bit.application;
+
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
